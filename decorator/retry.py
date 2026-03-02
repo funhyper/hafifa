8a0868b28ba56c4e9efa13e73f3767a636137941
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2026-03-02 15:50:20 +0200

    small cr
    Signed-off-by: AharonSambol <75334174+AharonSambol@users.noreply.github.com>

--- decorator/retry.py
+++ decorator/retry.py
@@ -1,9 +1,11 @@
 import random
 
 
+# CR: typing
 def retry(times):
     def decorator(func):
         def wrapper(*args, **kwargs):
+            # CR: nice use of _  :)
             for _ in range(times):
                 try:
                     val = func(*args, **kwargs)
@@ -11,11 +13,13 @@ def retry(times):
                     continue
                 else:
                     return val
-            return None
+            return None     # CR: how about raising the exception (if it failed even after the retry I'll want to know)
         return wrapper
     return decorator
 
+
 @retry(times=10)
+# CR: return type `-> None`
 def hello():
     x = random.random()
     if x < 0.1:
--- decorator/timer.py
+++ decorator/timer.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 def timer(func):
     def wrapper(*args, **kwargs):
+        # CR: datetime is a bit overkill for this, I would use time.time instead
+        #   sidenote - time.perf_counter is used if you want to profile (just cool to know)
         start = datetime.now()
         val = func(*args, **kwargs)
         end = datetime.now()
@@ -11,9 +13,11 @@ def timer(func):
         return val
     return wrapper
 
+
 @timer
 def hello():
     time.sleep(1)
     print("nah")
+
 
 hello()
