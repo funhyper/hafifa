71d8b8ecec731c7882fdd25085c1f1d64bd238b0
funhyper <alonybbb@gmail.com>
2026-03-26 15:43:59 +0200

    Added mud tile

---
+++ src/code/bl/tiles/mud_tile.py
@@ -0,0 +1,17 @@
+from code.bl.tiles.abstract.tile_base import TileBase
+from code.bl.util.move import Move
+from code.bl.util.move_calculator import MoveCalculator
+from code.bl.util.point import Point
+
+
+class MudTile(TileBase):
+    def __init__(self, move_calculator: MoveCalculator, entity: EntityBase = None, moves_stuck=1):
+        super().__init__(entity)
+        self.move_calculator = move_calculator
+        self.moves_stuck = moves_stuck
+
+    def get_point_after_move(self, player_point: Point, move: Move) -> Point:
+        if self.moves_stuck == 0:
+            return self.move_calculator.get_point_after_move(player_point, move)
+        self.moves_stuck -= 1
+        return player_point
