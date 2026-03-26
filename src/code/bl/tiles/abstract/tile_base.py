edd2848305c903ddde291ce6a9d8c44b68f0f634
funhyper <alonybbb@gmail.com>
2026-03-26 12:55:17 +0200

    Added tile_base abstract class

---
+++ src/code/bl/tiles/abstract/tile_base.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from code.bl.util.move import Move
+from code.bl.util.point import Point
+
+
+class TileBase(ABC):
+    def __init__(self, entity: EntityBase = None):
+        self.entity = entity
+
+    @abstractmethod
+    def get_after_move_point(self, tile_point: Point, move: Move) -> Point:
+        raise NotImplementedError
