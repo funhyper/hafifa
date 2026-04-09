083f06a39a7e351cf8e10dd82b2b47200677d60d
funhyper <alonybbb@gmail.com>
2026-04-09 16:18:11 +0300

    Added board generator

---
+++ src/code/bl/board_provider/board_generator_provider.py
@@ -0,0 +1,16 @@
+from code.bl.board_provider.abstract.board_provider_base import BoardProviderBase
+from code.bl.game_board.game_board import GameBoard
+from code.bl.util.point import Point
+
+
+class BoardGeneratorProvider(BoardProviderBase):
+    def __init__(self, tile_generator, board_size: Point):
+        self.tile_generator = tile_generator
+        self.board_size = board_size
+
+    def get_board(self) -> GameBoard:
+        board = dict()
+        for x in range(self.board_size.x):
+            for y in range(self.board_size.y):
+                board[Point(x, y)] = self.tile_generator.get_random_tile()
+        return GameBoard(board)
